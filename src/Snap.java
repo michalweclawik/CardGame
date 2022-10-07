96d4533ed96515f7c7e9a7fb7a084e4474b351e4
michal <michalwagh@gmail.com>
2022-10-07 10:33:19 +0100

    cleaning code

--- src/Snap.java
+++ src/Snap.java
@@ -4,10 +4,9 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class Snap extends CardGame {
-    static Scanner userInput = new Scanner(System.in);
+
     Timer timer = new Timer();
     public void runGame() {
-
 
         shuffleDeck();
         String previousSymbol = null;
@@ -15,47 +14,50 @@ public class Snap extends CardGame {
         boolean hasToRun = true;
 
         while (hasToRun) {
-
-
             System.out.println( "Player " + Player.player  + " please press enter");
+
+
             Scanner scanner = new Scanner(System.in);
             scanner.nextLine();
+
             Card currentCard = dealCard();
             currentSymbol = currentCard.getSymbol();
-
             System.out.println(currentCard);
 
             Player.switchPlayer();
 
         if (Objects.equals(previousSymbol, currentSymbol)) {
-
-            System.out.println("Player: " + Player.player  + " type SNAP to win ");
-
-            TimerTask snap = new TimerTask() {
-                public void run() {
-//
-                    System.exit(0);
-
-                }
-            };
-
-            timer.schedule( snap, 2*1000 );
-
-            if((scanner.nextLine().equals("snap"))){
-
-                System.out.println("Game finish player: " + Player.player  + " won!!!!! ");
-            } else {
-
-                System.out.println("Game finish player: " + Player.player  + " lost!!! ");
-                    Player.switchPlayer();
-                System.out.println("Game finish player: " + Player.player  + " won!!!!! ");}
-             hasToRun = false;
+            checkForEnd();
+            hasToRun = false;
         }
-
 
         previousSymbol = currentSymbol;
         currentSymbol=null;
     }timer.cancel();}
 
+
+
+public void checkForEnd(){Scanner userInput = new Scanner(System.in);
+    System.out.println("Player: " + Player.player  + " type SNAP to win ");
+    TimerTask snap = new TimerTask() {
+        public void run() {
+
+            System.out.println("Game finish player: " + Player.player  + " lost!!! ");
+            Player.switchPlayer();
+            System.out.println("Game finish player: " + Player.player  + " won!!!!! ");
+            System.exit(0);
+        }
+    };
+    timer.schedule( snap, 2000 );
+
+    if((userInput.nextLine().equalsIgnoreCase("snap"))){
+
+        System.out.println("Game finish player: " + Player.player  + " won!!!!! ");
+    } else {
+
+        System.out.println("Game finish player: " + Player.player  + " lost!!! ");
+        Player.switchPlayer();
+        System.out.println("Game finish player: " + Player.player  + " won!!!!! ");}
+  }
 
 }
